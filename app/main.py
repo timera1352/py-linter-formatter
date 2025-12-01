900a98ecd35a1e19ddc86406f4d771d9698c1ad6
Buzsaki Bence <timera1352@gmail.com>
2025-12-01 19:55:03 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,8 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
-
+    return(format_linter_error(error=error))
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
-
+    return(format_single_linter_file(file_path="./source_code_2.py", errors=errors))
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return(format_linter_report(linter_report=linter_report))
